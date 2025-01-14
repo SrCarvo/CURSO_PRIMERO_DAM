37476165ae3f1c7184b541df851e8fcceadabaf2
SrCarvo <tomaandreiadelin@gmail.com>
2025-01-14 03:50:59 +0100

    ejercicio c#

---
+++ PROGRAMACION/C#/1- PruebasUnitarias/1 - TestPruebasUnitarias/TestCalculadora.cs
@@ -0,0 +1,96 @@
+﻿
+using _1__PruebasUnitarias;
+using NUnit.Framework;
+
+namespace _1___TestPruebasUnitarias
+{
+    [TestFixture]
+    public class TestCalculadora
+    {
+        private Calculadora _calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calc = new Calculadora();
+        }
+
+        [Test]
+        public void Sumar_DosNumeros_ReturnCorrect()
+        {
+            // Arrange - declarar las variables
+            int n1 = 2;
+            int n2 = 3;
+
+            // Act - metodo a probar - sumar y el resultado
+            // lo guardamos en la variable result
+            int result = _calc.Sumar(n1, n2);
+
+            // Assert (ver metodo equals de ASSERT, explica como usar THAT)
+            //2- ver clase IS
+            Assert.That<int>(result, Is.EqualTo(5));
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(1, -2)]
+        [TestCase(1, 3)]
+        public void Sumar_DosNumeros_ReturnCorrectP2(int n1, int n2)
+        {
+
+            // Act - metodo a probar - sumar y el resultado
+            // lo guardamos en la variable result
+            int result = _calc.Sumar(n1, n2);
+
+            // Assert (ver metodo equals de ASSERT, explica como usar THAT)
+            //2- ver clase IS
+            Assert.That<int>(result, Is.EqualTo(n1 + n2));
+        }
+
+        [Test]
+        public void Restar_DosNumeros_ReturnCorrect()
+        {
+            // Arrange - declarar las variables
+            int n1 = 5;
+            int n2 = 3;
+
+            // Act - metodo a probar 
+            int result = _calc.Restar(n1, n2);
+
+            // Assert (ver metodo equals de ASSERT, explica como usar THAT)
+            //2- ver clase IS
+            Assert.That<int>(result, Is.EqualTo(2));
+        }
+
+        [TestCase("Pass123!", true)]
+        [TestCase("123456", false)]
+        [TestCase("password", false)]
+        [TestCase("PASSWORD123", false)]
+        [TestCase("Password123!", false)] // Contraseña que excede el límite de 12 caracteres
+        [TestCase("Pass1234!", true)]
+        public void ComprobarContraseña_ValidarContraseña_ReturnCorrect(string contraseña, bool expectedResult)
+        {
+            // Act - metodo a probar
+            bool result = _calc.ComprobarContraseña(contraseña);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
+
+
+
+//[Test]
+//public void Restar_CuandoRestaDosNumeros_ReturnCorrect(int n1, int n2)
+//{
+//    //// Arrange
+//    //int numero1 = 10;
+//    //int numero2 = 4;
+
+//    // Act
+//    int result = _calculadoraTest.Restar(n1, n2);
+
+//    // Assert
+//    Assert.That(result, Is.True, $@"El resultado debe ser la resta de {n1} y {n2}");
+//}
+
---
+++ PROGRAMACION/C#/1- PruebasUnitarias/1- PruebasUnitarias/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1__PruebasUnitarias
+{
+    public class Calculadora
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+
+        public bool ComprobarContraseña(string contraseña)
+        {
+            // Verificar que el tamaño sea menor que 12
+            if (contraseña.Length >= 12)
+            {
+                return false;
+            }
+
+            // Verificar que contenga al menos un número
+            bool contieneNumero = contraseña.Any(char.IsDigit);
+
+            // Verificar que contenga al menos una letra mayúscula
+            bool contieneMayuscula = contraseña.Any(char.IsUpper);
+
+            // Verificar que contenga al menos un acarcter especial
+            bool contieneSimbol = contraseña.Any(char.IsSymbol) || contraseña.Any(char.IsPunctuation);
+
+
+            // Retornar verdadero solo si cumple todas las condiciones
+            return contieneNumero && contieneMayuscula && contieneSimbol;
+        }
+    }
+}
